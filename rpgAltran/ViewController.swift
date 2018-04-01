54f545d159bcaa5a8894c07b6f3b31213cafd2b6
José Gabriel Ferrer Morales <jgabriel.ferrer@gmail.com>
2018-04-01 15:41:55 +0200

    Initial Commit

---
+++ rpgAltran/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rpgAltran
+//
+//  Created by Jose Gabriel Ferrer on 1/4/18.
+//  Copyright © 2018 Jose Gabriel Ferrer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
